b8391d0e166ce1501a8670be9e53dd1d9afd11d0
Srikant Kumar <52560067+imsrikant@users.noreply.github.com>
2024-03-18 20:15:28 +0000

    Add about and contact paths to urls.py and update nav.html

--- hello_world/core/urls.py
+++ hello_world/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from hello_world.core.views import index, faq
+from hello_world.core.views import index, faq, about, contact
 
 urlpatterns = [
     path("", index, name="index"),
     path("faq/", faq, name="faq"),
+    path("about/", about, name="about"),
+    path("contact/", contact, name="contact"),
 ]
