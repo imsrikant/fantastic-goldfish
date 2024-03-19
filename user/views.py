5ef5ea12bc57b75a6bcf83c4975565be7de2b2aa
Srikant Kumar <52560067+imsrikant@users.noreply.github.com>
2024-03-19 06:21:20 +0000

    Add error handling in signup and login forms

--- user/views.py
+++ user/views.py
@@ -5,12 +5,48 @@ from .models import User
 
 def user_signup(request):
     signup_context = {"title": "Sign Up"}
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+        # Check if username or email already exists
+        if User.objects.filter(username=username).exists():
+            signup_context["error"] = "Username already exists"
+        elif User.objects.filter(email=email).exists():
+            signup_context["error"] = "Email already exists"
+        elif password == confirm_password:
+            user = User(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password,
+            )
+            user.save()
+            return redirect("user_login")
+        else:
+            signup_context["error"] = "Password and Confirm Password do not match"
     return render(request, "user/signup.html", signup_context)
 
 
 def user_login(request):
     login_context = {"title": "Login"}
-
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        # Check if user exists and password is correct
+        try:
+            user = User.objects.get(email=email)
+            if user.password == password:
+                # Redirect to home
+                return redirect("/")
+            else:
+                login_context["error"] = "Incorrect password"
+        except User.DoesNotExist:
+            login_context["error"] = "User does not exist"
     return render(request, "user/login.html", login_context)
 
 
