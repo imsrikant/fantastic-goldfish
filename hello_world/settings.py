933244f02e1de58f944c03e2d0db877b4aaf0d77
Srikant Kumar <52560067+imsrikant@users.noreply.github.com>
2024-03-19 19:16:02 +0000

    Pending changes exported from your codespace

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -145,3 +145,4 @@ MEDIA_ROOT = BASE_DIR / "hello_world" / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "user.User"
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,52 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import User
 
-# Register your models here.
+# # Register your models here.
+
+
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ("username", "email", "user_type", "is_staff")
+#     search_fields = ("username", "email")
+#     list_filter = ("user_type", "is_staff")
+#     ordering = ("username",)
+#     filter_horizontal = ()
+#     fieldsets = (
+#         (None, {"fields": ("username", "password")}),
+#         ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+#         (
+#             "Permissions",
+#             {
+#                 "fields": (
+#                     "is_active",
+#                     "is_staff",
+#                     "is_superuser",
+#                     "groups",
+#                     "user_permissions",
+#                 )
+#             },
+#         ),
+#         ("Important dates", {"fields": ("last_login", "date_joined")}),
+#     )
+#     add_fieldsets = (
+#         (
+#             None,
+#             {
+#                 "classes": ("wide",),
+#                 "fields": ("username", "email", "password1", "password2"),
+#             },
+#         ),
+#     )
+#     readonly_fields = ("last_login", "date_joined")
+#     filter_horizontal = ("groups", "user_permissions")
+#     list_per_page = 20
+#     save_on_top = True
+#     actions_on_top = True
+#     actions_on_bottom = True
+#     empty_value_display = "-empty-"
+#     show_full_result_count = True
+#     show_all = True
+#     show_change_link = True
+#     show_delete_link = True
+#     show_save = True
+#     show_save_as_new = True
