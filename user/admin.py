a15c44e741cdf0544fb39c60909e07f0fed5f411
Srikant Kumar <52560067+imsrikant@users.noreply.github.com>
2024-03-19 12:24:51 +0000

    Delete unused files and update user forms

--- user/admin.py
+++ user/admin.py
@@ -1,14 +1,32 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Orphanage, Skill, UserSkill, Problem
 # Register your models here.
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'username', 'email']
-    search_fields = ['first_name', 'last_name', 'username', 'email']
-    list_filter = ['first_name', 'last_name', 'username', 'email']
-    list_per_page = 10
-    list_max_show_all = 100
-    list_select_related = False
-    list_display_links = ['first_name', 'last_name', 'username', 'email']
-    list_editable = []
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    list_filter = ("is_staff", "is_active")
+
+@admin.register(Orphanage)
+class OrphanageAdmin(admin.ModelAdmin):
+    list_display = ("organization_name", "location", "contact_email")
+    search_fields = ("organization_name", "location", "contact_email")
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ("skill_name", "skill_category")
+    search_fields = ("skill_name", "skill_category")
+
+@admin.register(UserSkill)
+class UserSkillAdmin(admin.ModelAdmin):
+    list_display = ("user", "skill")
+    search_fields = ("user", "skill")
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ("orphanage", "title", "status")
+    search_fields = ("orphanage", "title", "status")
+    list_filter = ("status",)
+
+
