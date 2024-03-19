15c039f3a00b070474747070fdcb7dbdfbd9bdde
Srikant Kumar <52560067+imsrikant@users.noreply.github.com>
2024-03-19 04:59:51 +0000

    Update links in HTML templates and add forgot password functionality

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import user_signup, user_login
+from .views import user_signup, user_login, user_forgot_password
 
 urlpatterns = [
     path("signup/", user_signup, name="user_signup"),
     path("login/", user_login, name="user_login"),
+    path("forgot password/", user_forgot_password, name="user_forgot_password"),
 ]
-
--- user/views.py
+++ user/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def user_signup(request):
     signup_context = {"title": "Sign Up"}
     print(request.user)
     return render(request, "user/signup.html", signup_context)
 
+
 def user_login(request):
     login_context = {"title": "Login"}
     return render(request, "user/login.html", login_context)
+
+
+def user_forgot_password(request):
+    forgot_password_context = {"title": "Forgot Password"}
+    return render(request, "user/forgot-password.html", forgot_password_context)
