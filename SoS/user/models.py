19645680554409d95f8fe8291b897c1be1c1edb8
Srikant Kumar <52560067+imsrikant@users.noreply.github.com>
2024-03-19 12:27:58 +0000

    Add User model to admin site

--- SoS/user/models.py
+++ SoS/user/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ("orphanage", "Orphanage"),
+        ("volunteer", "Volunteer"),
+    )
+
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    contact_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
